7c74f5e04d4eecbbec0b3987ddc9dae8f3cf81eb
Gabriel Alves <bielalvesferreira2003@gmail.com>
2023-08-08 20:18:23 -0300

    projeto final

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,16 +4,6 @@
 
 export const environment = {
   production: false,
-  apiMapsKey: 'AIzaSyBIpKQL-Ho3wnd1UjiM4cuqppvQLv2roHk',
-  firebaseConfig: {
-    apiKey: 'AIzaSyAbWnrAPl1VvdIPiXF1urJ4yLkQoVlpxcM',
-    authDomain: 'st-wiki-fb5e7.firebaseapp.com',
-    projectId: 'st-wiki-fb5e7',
-    storageBucket: 'st-wiki-fb5e7.appspot.com',
-    messagingSenderId: '1024563364425',
-    appId: '1:1024563364425:web:9f1fffc4c479ed3677f598',
-    measurementId: 'G-7MTB9NCD74',
-  },
 };
 
 /*
