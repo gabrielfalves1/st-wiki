9966ba53dfef33b39ee8102a1122834a506af645
Gabriel Alves <103003179+gabrielfalves1@users.noreply.github.com>
2023-08-22 14:27:43 -0300

    Create environment.prod.ts

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  apiMapsKey: 'Preencha com sua key da api do google maps',
+
+  firebaseConfig: {
+ //Preencha suas credenciais do firebase
+  },
+};
